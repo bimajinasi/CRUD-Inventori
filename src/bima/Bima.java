93870e9c8a8787f087cf43f36bcfb407c0226fa2
B <64383937+bimajinasi@users.noreply.github.com>
2021-02-08 18:33:01 +0700

    Add files via upload

---
+++ src/bima/Bima.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bima;
+
+/**
+ *
+ * @author Bima
+ */
+public class Bima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrameEbook fe = new FrameEbook();
+        fe.setVisible(true);
+    }
+    
+}
